9b0c7df7805aa07d70ba41840e84915e42db37c2
SK5277 <singhsushil5277@gmaiil.com>
2018-07-03 18:05:44 +0530

    [MOD] modify MySQL query

--- SK/BeastPython/OnlineDataStore.py
+++ SK/BeastPython/OnlineDataStore.py
@@ -31,21 +31,42 @@ def getLocation():
     c2.execute("Select ip,ip_id from xp_location where ip_id>'"+str(old_count)+"' AND ip_id<='"+str(new_count)+"'")
     for row in c2:    
         url = "http://api.ipstack.com/"+row[0].strip()+"?access_key=2f8c01c0484028dc8cb07748187bc350"
-        
+        row = str(row[0])
+        print(row)
         myResponse = requests.get(url)
         if(myResponse.ok):
             jData = json.loads(myResponse.content)
+        
+            ip= 'no' if jData['ip'] ==None else jData['ip']
+            jtype = 'no' if jData['type'] ==None else jData['type']
+            continent_code= 'no' if jData['continent_code'] ==None else jData['continent_code']
+            continent_name='no' if jData['continent_name'] ==None else jData['continent_name']
+            
+            country_code= 'no' if jData['country_code'] ==None else jData['country_code']
+            country_name= 'no' if jData['country_name'] ==None else jData['country_name']
+
+            region_code = 'no' if jData['region_code'] ==None else jData['region_code']
+            region_name = 'no' if jData['region_name'] ==None else jData['region_name']
+                            
+            city = 'no' if jData['city'] ==None else jData['city']
+            jzip = 'no' if jData['zip'] ==None else jData['zip']
+
+            latitude = 'no' if jData['latitude'] ==None else jData['latitude']
+            longitude = 'no' if jData['longitude'] ==None else jData['longitude']
+            
             sql = "UPDATE xp_location SET \
-                    ip='"+jData['ip']+"',\
-                    type='"+jData['type']+"',\
-                    continent_code='"+jData['continent_code']+"',\
-                    continent_name='"+jData['continent_name']+"',\
-                    country_code='"+jData['country_code']+"',\
-                    country_name='"+jData['country_name']+"',\
-                    region_code='"+jData['region_code']+"',\
-                    region_name='"+jData['region_name']+"',\
-                    city='"+jData['city']+"',\
-                    zip='"+jData['zip']+"', latitude='"+str(jData['latitude'])+"',longitude='"+str(jData['longitude'])+"' WHERE ip='"+str(row[0]).strip()+"'"
+                    ip='"+str(ip)+"',\
+                    type='"+str(jtype)+"',\
+                    continent_code='"+str(continent_code)+"',\
+                    continent_name='"+str(continent_name)+"',\
+                    country_code='"+str(country_code)+"',\
+                    country_name='"+str(country_name)+"',\
+                    region_code='"+str(region_code)+"',\
+                    region_name='"+str(region_name)+"',\
+                    city='"+str(city)+"',\
+                    zip='"+str(jzip)+"',\
+                    latitude='"+str(latitude)+"',\
+                    longitude="+str(longitude)+" WHERE ip='"+str(row)+"'"
             print(sql)
             c3 = mydb.cursor()
             c3.execute(sql)
@@ -62,4 +83,3 @@ while True:
         getLocation()
     except:
         getLocation()
-        
